3f650ef31c166ae9ec9218a8996e33915c7371a2
Łukasz Kurczab <lukasz.kurczab@gmail.com>
2024-03-15 21:28:24 +0100

    addHistoryItemToUser method created

--- routes/users.js
+++ routes/users.js
@@ -4,44 +4,46 @@ const cosmosConfigModule = require('../cosmosConfig');
 
 const queryUsers = async () => {
   const container = await cosmosConfigModule.getUsersContainer();
-  const {resources} = await container.items.readAll().fetchAll();
+  const { resources } = await container.items.readAll().fetchAll();
   return resources;
-}
+};
 
-const getUser = async (userId) => {
+const getUser = async userId => {
   const container = await cosmosConfigModule.getUsersContainer();
   const response = await container.item(userId, 'test@example.com').read();
   return response.resource;
-}
+};
 
-const addHistoryItemToUsers = async (userId, historyItemId) => {
+const addHistoryItemToUsers = async (userId, historyItem) => {
   const container = await cosmosConfigModule.getUsersContainer();
-  await container.item(userId).history.push({id: historyItemId});
-  return container.item(userId).history;
-}
+  const user = await container.item(userId, 'test@example.com').read();
+  const operation = [{ op: 'add', path: '/history', value: [...user.resource.history, historyItem] }];
+  await container.item(userId, 'test@example.com').patch(operation);
+  return historyItem;
+};
 
 const addPlanToUsers = async (userId, planId) => {
   const container = await cosmosConfigModule.getUsersContainer();
-  await container.item(userId).plans.push({id: planId});
+  await container.item(userId).plans.push({ id: planId });
   return container.item(userId).plans;
-}
+};
 
 router.get('/', async (req, res, next) => {
-  try{
+  try {
     const items = await queryUsers();
     res.send(items);
-  } catch(err) {
-    console.log(err.message)
+  } catch (err) {
+    console.log(err.message);
     res.status(500).send(err.message);
   }
 });
 
 router.get('/:id', async (req, res, next) => {
-  try{
+  try {
     const items = await getUser(req.params.id);
     res.send(items);
-  } catch(err) {
-    console.log(err.message)
+  } catch (err) {
+    console.log(err.message);
     res.status(500).send(err.message);
   }
 });
@@ -49,8 +51,8 @@ router.get('/:id', async (req, res, next) => {
 router.put('/history/:id', async (req, res, next) => {
   try {
     const userId = req.params.id;
-    const historyItemId = req.body;
-    const updatedUserHistoryItemsInDb = await addHistoryItemToUsers(userId, historyItemId);
+    const historyItem = req.body;
+    const updatedUserHistoryItemsInDb = await addHistoryItemToUsers(userId, historyItem);
     res.send(updatedUserHistoryItemsInDb);
   } catch (err) {
     res.status(500).send(err.message);
